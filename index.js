d9723399491b4ddddc32ffae6ddcd229bc5dbaf9
Piyush1532 <piyushadav503@gmail.com>
2024-11-15 16:12:04 +0530

    nodeNoteApp

---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const fs = require("fs");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+const Port = 4100;
+
+app.get("/", (req, res) => {
+  fs.readdir(`./files`, (err, files) => {
+    res.render("index", { files: files });
+  });
+});
+
+app.get("/file/:filename", (req, res) => {
+  fs.readFile(`./files/${req.params.filename}`, "utf-8", (err, fileData) => {
+    res.render("show", { filename: req.params.filename, filedata: fileData });
+  });
+});
+
+app.get("/edit/:filename", (req, res) => {
+  res.render("edit", { filename: req.params.filename });
+});
+
+app.post("/edit", (req, res) => {
+  fs.rename(
+    `./files/${req.body.prevTitle}`,
+    `./files/${req.body.newTitle}`,
+    function (err) {
+      res.redirect("/");
+    }
+  );
+});
+
+app.post("/create", (req, res) => {
+  fs.writeFile(
+    `./files/${req.body.title.split(" ").join("")}.txt`,
+    req.body.description,
+    (err) => {
+      res.redirect("/");
+    }
+  );
+});
+
+app.post("/file/delete", (req, res) => {
+  const filePath = `./files/${req.body.filename}`;
+  fs.unlink(filePath, (err) => {
+    if (err) {
+      console.error("Error deleting file:", err);
+      return res.status(500).send("Failed to delete file.");
+    }
+    res.redirect("/");
+  });
+});
+
+
+app.listen(Port, () => {
+  console.log(`server started on port ${Port}`);
+});
